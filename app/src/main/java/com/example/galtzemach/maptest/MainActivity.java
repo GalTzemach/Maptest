82a14be02ced7785bdb38afb1535f0d830f2fa3c
Gal Tzemach <galtzemach@gmail.com>
2017-01-08 18:36:07 +0200

    int x = 10; //gal

--- app/src/main/java/com/example/galtzemach/maptest/MainActivity.java
+++ app/src/main/java/com/example/galtzemach/maptest/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        int x = 0;
+        int x = 10;
+        //gal
 
     }
 
